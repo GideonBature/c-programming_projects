5daddfa212c182443f7f4b69bd8b12782e25c5a3
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-25 19:11:47 +0100

    fn prints lowercase alphabet 10x

---
+++ 13-print_alphabet_10x/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls the print_alphabet function
+ * Return: Always 0.
+*/
+int main(void)
+{
+    print_alphabet_10x();
+
+    return (0);
+}
+
---
+++ 13-print_alphabet_10x/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+void print_alphabet_10x(void);
+
+#endif /* _MAIN_H */
+
