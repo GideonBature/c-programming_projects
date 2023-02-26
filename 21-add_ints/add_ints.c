9ffe980569c02d917a786395bf657a75c7962313
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-26 09:44:35 +0100

    fn adds two ints and returns result

---
+++ 21-add_ints/add_ints.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add_ints - add two ints and return result
+ * Return: Always result (success)
+*/
+int add_ints(int x, int y)
+{
+	int res;
+
+	res = x + y;
+
+	return (res);
+}
---
+++ 21-add_ints/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+int add_ints(int, int);
+
+#endif /* _MAIN_H */
