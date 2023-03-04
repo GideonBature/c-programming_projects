213c1b3f88bd60b85ff2f00a006820b47475e06e
Gideon Bature <infoaboutgideon@gmail.com>
2023-03-04 08:20:35 +0100

    fn multiplies two integers

---
+++ 30-mul/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+int mul(int a, int b);
+#endif /* _MAIN_H */
---
+++ 30-mul/mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * Return: Always 0
+*/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
